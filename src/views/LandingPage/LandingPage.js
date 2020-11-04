77163b13f8abbdceb3a4a9495081eaed81c93ef8
Brian Musial <brian.musial@gmail.com>
2020-11-04 15:00:24 -0500

    adding landing page

--- src/views/LandingPage/LandingPage.js
+++ src/views/LandingPage/LandingPage.js
@@ -1,49 +1,49 @@
-import React from "react";
+import React from 'react'
 // nodejs library that concatenates classes
-import classNames from "classnames";
+import classNames from 'classnames'
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles'
 
 // @material-ui/icons
 
 // core components
-import Header from "components/Header/Header.js";
-import Footer from "components/Footer/Footer.js";
-import GridContainer from "components/Grid/GridContainer.js";
-import GridItem from "components/Grid/GridItem.js";
-import Button from "components/CustomButtons/Button.js";
-import HeaderLinks from "components/Header/HeaderLinks.js";
-import Parallax from "components/Parallax/Parallax.js";
+import Header from 'components/Header/Header.js'
+import Footer from 'components/Footer/Footer.js'
+import GridContainer from 'components/Grid/GridContainer.js'
+import GridItem from 'components/Grid/GridItem.js'
+import Button from 'components/CustomButtons/Button.js'
+import HeaderLinks from 'components/Header/HeaderLinks.js'
+import Parallax from 'components/Parallax/Parallax.js'
 
-import styles from "assets/jss/material-kit-react/views/landingPage.js";
+import styles from 'assets/jss/material-kit-react/views/landingPage.js'
 
 // Sections for this page
-import ProductSection from "./Sections/ProductSection.js";
-import TeamSection from "./Sections/TeamSection.js";
-import WorkSection from "./Sections/WorkSection.js";
+import ProductSection from './Sections/ProductSection.js'
+import TeamSection from './Sections/TeamSection.js'
+import WorkSection from './Sections/WorkSection.js'
 
-const dashboardRoutes = [];
+const dashboardRoutes = []
 
-const useStyles = makeStyles(styles);
+const useStyles = makeStyles(styles)
 
 export default function LandingPage(props) {
-  const classes = useStyles();
-  const { ...rest } = props;
+  const classes = useStyles()
+  const { ...rest } = props
   return (
     <div>
       <Header
         color="transparent"
         routes={dashboardRoutes}
-        brand="Material Kit React"
+        brand="Material Kit React!"
         rightLinks={<HeaderLinks />}
         fixed
         changeColorOnScroll={{
           height: 400,
-          color: "white"
+          color: 'white',
         }}
         {...rest}
       />
-      <Parallax filter image={require("assets/img/landing-bg.jpg")}>
+      <Parallax filter image={require('assets/img/landing-bg.jpg')}>
         <div className={classes.container}>
           <GridContainer>
             <GridItem xs={12} sm={12} md={6}>
@@ -78,5 +78,5 @@ export default function LandingPage(props) {
       </div>
       <Footer />
     </div>
-  );
+  )
 }
